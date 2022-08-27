330a340775dbe352be9606a04a583bde3982bc93
Carlos Filipe Paiva da Cunha Macieira Antunes <kucaantunes@hotmail.com>
2022-08-27 22:09:50 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+//$CFG->reverseproxy = 'true';
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'mysql';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+//$CFG->wwwroot   = 'http://95.95.66.83';
+$CFG->wwwroot   = 'http://127.0.0.1';
+$CFG->dataroot  = 'D:\\Moodle\\server\\moodledata';
+$CFG->admin     = 'admin';
+
+
+$CFG->directorypermissions = 0777;
+//$CFG->directorypermissions = 0077;
+
+
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
